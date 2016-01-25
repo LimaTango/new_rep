bae9800729e8bd898906d885a59e4ad69c0a55dc
limatango <lxthetai@gmail.com>
2016-01-25 11:24:12 +0100

    complete code

--- elevation_api.py
+++ elevation_api.py
@@ -1,52 +1,38 @@
 _author__ = 'tai'
 
 from matplotlib import pyplot as plt
-import matplotlib.mlab as ml
 import simplejson
 import urllib
 import numpy as np
-f = open('elevation_data.txt', 'w')
+# f = open('elevation_data.txt', 'w')
 
-ELEVATION_BASE_URL = 'http://maps.google.com/maps/api/elevation/json'
+ELEVATION_BASE_URL = 'http://maps.google.com/maps/api/elevation/json?path='
+key = '&key=AIzaSyCtsxFi2UFr6Cr4rWLFsviWXOvhEh1UEsk'
 
 
-def get_elevation(path="36.64,-116.83171|36.24,-116.83171", samples="100", sensor="false", **elvtn_args):
-    elvtn_args.update({
-        'path': path,
-        'samples': samples,
-        'sensor': sensor
-    })
-    url = ELEVATION_BASE_URL + '?' + urllib.urlencode(elvtn_args)
+def get_elevation(path, samples):
+    url = "https://maps.googleapis.com/maps/api/elevation/json?path=" + path + "&samples=400&key=AIzaSyCtsxFi2UFr6Cr4rWLFsviWXOvhEh1UEsk"
     response = simplejson.load(urllib.urlopen(url))
     elevation_array = []
     for result_set in response['results']:
+        if result_set['elevation'] < 0:
+            result_set['elevation'] = 0
         elevation_array.append(result_set['elevation'])
-        #f.write(str(result_set['elevation']) + '\n')
+    print response["status"]
     return elevation_array
 
 if __name__ == '__main__':
-
-    #startStr = raw_input('Enter the start latitude,longitude value (default Mt. Whitney) --> ').replace(' ','')
-    #if not startStr:
-    # startStr = "36.578581,-118.291994"
-
-    #endStr = raw_input('Enter the end latitude,longitude value (default Death Valley) --> ').replace(' ','')
-    #if not endStr:
-    # endStr = "36.23998,-116.83171"
     elevation_list = []
-    start_lat = "51.8836,"
-    end_lat = "52.0836,"
-    for i in range(30):
-        longitude = 15.4 + 0.001*i
-        start_str = start_lat + str(longitude)
-        end_str = end_lat + str(longitude)
+    start_lat = "54.15,"
+    end_lat = "54.55,"
+    lng_list = np.linspace(18.46, 15.86, num=400)
+    for elt in lng_list:
+        lng = elt
+        start_str = start_lat + str(lng)
+        end_str = end_lat + str(lng)
         pathStr = start_str + "|" + end_str
-        elevation = get_elevation(pathStr, 30)
+        elevation = get_elevation(pathStr, "100")
         elevation_list.append(elevation)
-    long_list = np.linspace(15.4, 15.8, num=30)
-    lat_list = np.linspace(51.8836, 52.0836, num=30)
-    X, Y = np.meshgrid(long_list, lat_list)
-    print len(elevation_list[0]), len(X), len(Y)
-    #plt.contour(X, Y, elevation_list)
-    plt.contour(np.array(elevation_list))
+    plt.imshow(elevation_list)
     plt.show()
+
